69f0189ba926783fc9d9bc1b8826cea2ab31ced8
A'zamov Samandar <admin@jscorp.uz>
2025-07-10 18:33:52 +0500

    updated redirect url

--- core/apps/payment/services/paylov.py
+++ core/apps/payment/services/paylov.py
@@ -6,7 +6,7 @@ def generate_payment_link(amount, order_id):
     # Create query string
     base_url = "https://my.paylov.uz/checkout/create/"
     merchant_id = "ed364cd2-313c-4842-81e4-c9d74ccfb7b8"
-    return_url = "https://atomcom.jscorp.uz/api/paylov/"
+    return_url = "https://atomcom.framer.website/cart"
 
     query_params = {"merchant_id": merchant_id, "amount": amount, "return_url": return_url}
     query_params[f"account.order_id"] = order_id
